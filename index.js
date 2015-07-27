a93ee22165e3da99711437ed1a6027bd79e1f4a8
Stacy Gee <stacy.t.gee@gmail.com>
2015-07-27 02:51:59 -0700

    v2 left side navigation
    includes:
    - left sidebar w/ images for about me, resume, projects, blog, and
    contact
    - header and heading using jumbotron from bootstrap
    - content area currently empty
    
    Next steps:
    - onClick for sidebar navigation
    - content for each page

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
-/* index.js */
 function changeImage(){
-	alert("You clicked this image!");
-	if (document.getElementById("cuteStarWars").src == "https://cloud.githubusercontent.com/assets/8279096/8731463/59eb7fea-2bac-11e5-87ab-5d4def4677c9.JPG"){
-			document.getElementById("cuteStarWars").src = "https://cloud.githubusercontent.com/assets/8279096/8731464/59ef229e-2bac-11e5-9479-b90208b4f6dd.JPG";
-			alert("Success! Image changed!");
-		}
-	else{
-		document.getElementById("cuteStarWars").src = "https://cloud.githubusercontent.com/assets/8279096/8731463/59eb7fea-2bac-11e5-87ab-5d4def4677c9.JPG";
-		alert("Success! Image changed!");
-	}
- }
+ 				alert("You clicked this image!");
+ 				if (document.getElementById("cuteStarWars").src == "https://cloud.githubusercontent.com/assets/8279096/8731463/59eb7fea-2bac-11e5-87ab-5d4def4677c9.JPG"){
+ 						document.getElementById("cuteStarWars").src = "https://cloud.githubusercontent.com/assets/8279096/8731464/59ef229e-2bac-11e5-9479-b90208b4f6dd.JPG";
+ 						alert("Success! Image changed!");
+ 					}
+ 				else{
+ 					document.getElementById("cuteStarWars").src = "https://cloud.githubusercontent.com/assets/8279096/8731463/59eb7fea-2bac-11e5-87ab-5d4def4677c9.JPG";
+ 					alert("Success! Image changed!");
+ 				}
+ 			}
+
+/* shows the content within the content div */
+function showContent(){
+
+}
